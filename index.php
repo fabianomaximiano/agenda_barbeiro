358078b02468f7bb6afcafe4d0d07579068e3eb8
Fabiano Maximiano <fabianomaximiano@hotmail.com>
2023-03-17 20:02:10 -0300

    variaveis vazias

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
 
-    echo "meu projeto";
+    
+    if($nome == false){
+    $nome = $_POST['nome'];
+    //$telefone = $_POST['telefone'];
+    //$email = $_POST['email'];
+    //$cep = $_POST['cep'];
+    echo "nao tem nada aqui";
+}else{ 
+    if(isset($nome)){
+        echo '<p>variavel existe!</p>';
+    }else{
+        echo '<p>variavel não existe</p>';
+    }
+    echo '<hr>';
+    if(empty($nome)){
+        echo '<p>variavel vazia</p>';
+    }
+
+}
